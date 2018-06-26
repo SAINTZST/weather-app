0620a3b6c87722d43f821a5585d2882238a9f378
Tachakorn Uengpaporn <tachakorn.uengpaporn@gmail.com>
2018-06-27 01:01:26 +0700

    wiring up weather search

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
+// const request = require('request')
+// const yargs = require('yargs')
+// const geocode = require('./geocode/geocode')
+
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             description: 'Address to fetch weather',
+//             string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv
+
+// geocode.geocodeAddress(argv.a, (errorMessage, results) => {
+//     if (errorMessage) {
+//         console.log(errorMessage)
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2))
+//     }
+// })
+
+//5120ec19a4c30bb4734dfff1c4b0d444
+
 const request = require('request')
-const yargs = require('yargs')
-const geocode = require('./geocode/geocode')
 
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            description: 'Address to fetch weather',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv
+request({
+    url: 'https://api.darksky.net/forecast/5120ec19a4c30bb4734dfff1c4b0d444/13.5990961,100.5998319',
+    json: true
+}, (error, response, body) => {
 
-geocode.geocodeAddress(argv.a, (errorMessage, results) => {
-    if (errorMessage) {
-        console.log(errorMessage)
+    if (error) {
+        console.log('Unable to connect to darksky.net')
+    } else if (body.code === 400) {
+        console.log()
+        console.log(`Unable to fetch weather: ${body.error}`)
     } else {
-        console.log(JSON.stringify(results, undefined, 2))
+        console.log(body.currently.temperature)
     }
 })
