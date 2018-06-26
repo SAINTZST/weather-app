a18e493b31d5876d73446e9801553b4ca384fbe9
Tachakorn Uengpaporn <tachakorn.uengpaporn@gmail.com>
2018-06-26 10:29:07 +0700

    encoding user input and handle callback errors

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const request = require('request')
+const yargs = require('yargs')
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            description: 'Address to fetch weather',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv
+
+
+
+var adderss = encodeURIComponent(argv.a)
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Soi%20Mu%20Ban%20Rattanakorn%20Tambon%20Thai%20Ban%20Mai,%20Amphoe%20Mueang%20Samut%20Prakan%20Chang%20Wat%20Samut%20Prakan%2010280?key=https://maps.googleapis.com/maps/api/geocode/json?address=Soi%20Mu%20Ban%20Rattanakorn%20Tambon%20Thai%20Ban%20Mai,%20Amphoe%20Mueang%20Samut%20Prakan%20Chang%20Wat%20Samut%20Prakan%2010280?key=AIzaSyBZsPD9E-C67ek6419xd6Z2U4coCK7xS30',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${adderss}`,
     json: true
 }, (error, response, body) => {
-    var location = body.results[0].geometry.location
-    console.log(`Address : ${body.results[0].formatted_address}`)
-    console.log(`Latitude : ${location.lat}`)
-    console.log(`Latitude : ${location.lng}`)
+    if (error) {
+        console.log('Unable to connect to google servers.')
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that addresss.')
+    } else if (body.status === 'OK') {
+        var location = body.results[0].geometry.location
+        console.log(`Address : ${body.results[0].formatted_address}`)
+        console.log(`Latitude : ${location.lat}`)
+        console.log(`Latitude : ${location.lng}`)
+    }
+
 })
